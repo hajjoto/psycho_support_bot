5b65927d27014ed9dd761bbf26d395f144598956
hajjoto <124136441+hajjoto@users.noreply.github.com>
2026-05-16 19:40:46 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -4,4 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = os.getenv("ADMIN_ID")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if BOT_TOKEN is None:
+    raise ValueError("BOT_TOKEN not found")
+
+if ADMIN_ID is None:
+    raise ValueError("ADMIN_ID not found")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL not found")
+
+ADMIN_ID = int(ADMIN_ID)
