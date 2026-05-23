13f4ee1eeecf263ac14a981ba1bef00135b31f64
hajjoto <124136441+hajjoto@users.noreply.github.com>
2026-05-23 17:39:57 +0300

    Update states.py

--- states.py
+++ states.py
@@ -3,7 +3,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 class SupportDialog(StatesGroup):
     consent = State()
-
     open_problem = State()
     duration_check = State()
     trigger_check = State()
@@ -11,29 +10,23 @@ class SupportDialog(StatesGroup):
     scale_check = State()
     support_check = State()
     coping_check = State()
-
     tension_journal = State()
     action_choice = State()
-
     safety_check = State()
     feedback = State()
     stabilization_followup = State()
     second_feedback = State()
     additional_support = State()
     ready_to_finish = State()
-
     crisis_mode = State()
     finished = State()
-    
     therapy_flow = State()
     therapy_step = State()
-
     protocol_choice = State()
     protocol_running = State()
     protocol_feedback = State()
-
     start_time_choice = State()
-
     scenario_choice = State()
     psychoeducation = State()
     daily_advice = State()
+    review_waiting = State()
