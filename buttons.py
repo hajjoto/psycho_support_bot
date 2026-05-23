330a5c23c4f7dc42bdaddbb92cf4965963933fd0
hajjoto <124136441+hajjoto@users.noreply.github.com>
2026-05-23 16:47:26 +0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -11,6 +11,9 @@ BTN_NEW_EXERCISE = "Нова вправа"
 BTN_FINISH = "Завершити діалог"
 BTN_SHORT = "Коротка вправа"
 BTN_FULL = "Повна вправа"
+BTN_UNDERSTAND = "Зрозуміти свій стан"
+BTN_EXERCISES = "Перейти до вправ"
+BTN_DAILY_ADVICE = "Поради на день"
 
 
 def clean_button(text: str) -> str:
@@ -20,19 +23,22 @@ def clean_button(text: str) -> str:
     text = text.strip()
 
     buttons = [
-        BTN_START,
-        BTN_RESTART,
-        BTN_YES,
-        BTN_NO_FAST,
-        BTN_NEXT,
-        BTN_BETTER,
-        BTN_NOT_BETTER,
-        BTN_REPEAT,
-        BTN_ADVICE,
-        BTN_NEW_EXERCISE,
-        BTN_FINISH,
-        BTN_SHORT,
-        BTN_FULL,
+    BTN_START,
+    BTN_RESTART,
+    BTN_YES,
+    BTN_NO_FAST,
+    BTN_NEXT,
+    BTN_BETTER,
+    BTN_NOT_BETTER,
+    BTN_REPEAT,
+    BTN_ADVICE,
+    BTN_NEW_EXERCISE,
+    BTN_FINISH,
+    BTN_SHORT,
+    BTN_FULL,
+    BTN_UNDERSTAND,
+    BTN_EXERCISES,
+    BTN_DAILY_ADVICE,
     ]
 
     for button in buttons:
